fb9a3b23c5129b783fa3ec23edc8b4ae431c480e
Tao Hu <taohu88@gmail.com>
2019-02-02 09:38:39 -0800

    add setup.py

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # Tea
 
 Tea is a deep learning application library to make it easy to test ideas quick, to build model from end to end.
-It reuses the unix pipe ideas to connects the machine learning components, 
-from data preparation, augment, train, visualization, all together. 
 It is config driven so users write minimal code to try ideas.
 
 It will never to aim to be another deep learning framework such as pytorch, tensorflow. 
@@ -11,4 +9,3 @@ It is more like keras to make the end application building as easy as possible.
 It is easy to make things complicated, but it is hard to make things simple. As the old saying goes “大道至简”.
 Thus, it strives the best to make code understandable and maintainable even if we have to make code longer.
 It avoids unnecessary tricks whenever possible.
- 
---
+++ setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+#
+# Some basic setup for now
+#
+
+import os
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name = "tea",
+    version = "0.0.1",
+    author = "Tao Hu",
+    author_email = "taohu88@gmail.com",
+    description = ("Tea, a deep learning app framework, enables building deep learning apps fast"),
+    license = license,
+    keywords = "deep learning app framework",
+    url = "https://github.com/taohu88/tea",
+    packages=['teat'],
+    long_description=readme,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.6',
+        "Topic :: App framework",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
